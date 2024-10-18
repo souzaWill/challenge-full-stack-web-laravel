ee1a53d16c0cf098e44dee7e1c4ab863cdaf58d3
souzaWill <57758087+souzaWill@users.noreply.github.com>
2024-10-18 12:25:36 -0300

    feat: add model migration seed and factory of student model with some functions

---
+++ app/Models/Student.php
@@ -0,0 +1,27 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\BelongsTo;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    /**
+     * The attributes that are mass assignable.
+     *
+     * @var array<int, string>
+     */
+    protected $fillable = [
+        'document',
+        'registration_number',
+    ];
+
+    public function user(): BelongsTo
+    {
+        return $this->belongsTo(User::class);
+    }
+}
